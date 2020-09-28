1c0cf5bb3186a90f20cc7324fb6909a51e5075be
Walter Neto <wneto.macedo@gmail.com>
2020-09-28 18:22:15 -0500

    CSS Fixes

--- messenger/src/App.js
+++ messenger/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
+import { FormControl, Input } from '@material-ui/core';
 import './App.css';
 import Message from './Message';
 import db from './firebase';
 import firebase from 'firebase';
 import FlipMove from 'react-flip-move';
+import SendIcon from '@material-ui/icons/Send';
+import { IconButton } from '@material-ui/core';
 
 function App() {
   const [input, setInput] = useState('');
@@ -45,11 +47,12 @@ function App() {
       <h1>Messenger App</h1>
       <h2>Welcome {username}</h2>
 
-      <form>
-        <FormControl>
-          <InputLabel>Enter a message...</InputLabel>
-          <Input value={input} onChange={event => setInput(event.target.value)} />
-          <Button disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage}>Send Message</Button>
+      <form className="app__form">
+        <FormControl className="app__formControl">
+          <Input className="app__input" placeholder='Enter a message...' value={input} onChange={event => setInput(event.target.value)} />
+          <IconButton className="app__iconButton" disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage}>
+            <SendIcon />
+          </IconButton>
         </FormControl>
       </form>
 
